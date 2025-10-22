ba8fd1b76898abf63a8c473e698a87c249cbd4f3
Tjoennaas <145026954+Tjoennaas@users.noreply.github.com>
2025-10-22 07:19:54 +0200

    Første versjon av prosjektet

---
+++ Program.cs
@@ -0,0 +1,23 @@
+
+
+/*
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run(); */
+
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+var appName = builder.Configuration["Name"];
+
+var app = builder.Build();
+
+Console.WriteLine($"Starter applikasjonen: {appName}");
+app.MapGet("/", () => $"Velkommen til {appName}!");
+
+app.Run();
+
