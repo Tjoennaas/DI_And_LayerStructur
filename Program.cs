ea88971cf571844fcb61cd5b3d3e7d7f9249cb48
Tjoennaas <145026954+Tjoennaas@users.noreply.github.com>
2025-10-23 11:38:58 +0200

    Example built in middlewere

--- Program.cs
+++ Program.cs
@@ -1,19 +1,20 @@
 
  
+
+
+//oppg.1
 /*
-
-oppg.1
-
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+
 
 app.MapGet("/", () => "Hello World!");
-
 app.Run(); */
 
 
-// oppg.2
 
+
+// oppg.2
+/*
 var builder = WebApplication.CreateBuilder(args);
 var appName = builder.Configuration["Name"];
 
@@ -22,6 +23,7 @@ var app = builder.Build();
 Console.WriteLine($"Starter applikasjonen: {appName}");
 app.MapGet("/", () => $"Velkommen til {appName}!");
 
+app.run()
 
 
 //oppg.3 
@@ -30,16 +32,28 @@ app.MapGet("/", () => $"Velkommen til {appName}!");
 Middleware are components that handle HTTP requests and responses.
 They provide functionality such as logging, authentication, and error handling.
 They are added to the HTTP pipeline via Program.cs and executed in the order in which they are registered.
- */
+ 
 
 
-// Overview built in middlewere : https://learn.microsoft.com/en-us/aspnet/core/fundamentals/middleware/?view=aspnetcore-9.0#built-in-middleware
+ Overview built in middlewere : https://learn.microsoft.com/en-us/aspnet/core/fundamentals/middleware/?view=aspnetcore-9.0#built-in-middleware
 
- //app.UseRequestTiming();  
-app.UseHttpsRedirection();
+
+eks: */
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Required for UseAuthorization()
+builder.Services.AddAuthorization();
+var app = builder.Build();
+
+app.UseHttpsRedirection(); 
 app.UseAuthorization();
-//app.MapControllers();
+
+app.MapGet("/", () => "Hello World!");
 app.Run();
+
+
 
 
 
