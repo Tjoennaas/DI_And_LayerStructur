97d6169d1e1a7fea950722719a83b5c762eea6a2
Tjoennaas <145026954+Tjoennaas@users.noreply.github.com>
2025-10-23 10:38:42 +0200

    Adding built in middlewere

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 
-
+ 
 /*
+
+oppg.1
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
@@ -9,7 +12,7 @@ app.MapGet("/", () => "Hello World!");
 app.Run(); */
 
 
-
+// oppg.2
 
 var builder = WebApplication.CreateBuilder(args);
 var appName = builder.Configuration["Name"];
@@ -21,4 +24,23 @@ app.MapGet("/", () => $"Velkommen til {appName}!");
 
 
 
+//oppg.3 
+
+/*
+Middleware are components that handle HTTP requests and responses.
+They provide functionality such as logging, authentication, and error handling.
+They are added to the HTTP pipeline via Program.cs and executed in the order in which they are registered.
+ */
+
+
+// Overview built in middlewere : https://learn.microsoft.com/en-us/aspnet/core/fundamentals/middleware/?view=aspnetcore-9.0#built-in-middleware
+
+ //app.UseRequestTiming();  
+app.UseHttpsRedirection();
+app.UseAuthorization();
+//app.MapControllers();
 app.Run();
+
+
+
+
